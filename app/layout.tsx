d483b58c7da7083f9ce915bd216b52a7729f0e51
Emmanuel <en56434@gmail.com>
2025-12-11 17:57:58 +0100

    designed sidebar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,25 @@
 import "./globals.css";
+import "./globals.css";
+import { Cormorant, Cormorant_Infant, Cormorant_SC } from "next/font/google";
+
+const cor = Cormorant({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-cormorant",
+});
+
+const corInfant = Cormorant_Infant({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-cormorant-infant",
+});
+
+const corSC = Cormorant_SC({
+ weight : ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-cormorant-sc",
+});
 
 export default function RootLayout({
   children,
@@ -7,7 +28,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      {/* include both so the CSS variables are available site-wide */}
+      <body className={`${cor.className} ${corInfant.variable} ${corSC.variable}`}>
         {children}
       </body>
     </html>
--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,31 +1,54 @@
 import Image from 'next/image'
+import "./Sidebar.css"
+import closeIcon from "./assets/close-icon.svg"
+import placeholderImage from "./assets/placeholder-image.png"
+import bookmarkIcon from "./assets/bookmark-icon.svg"
+import speakerIcon from "./assets/speaker-icon.svg"
+import flagIcon from "./assets/flag.png"
+import bulbIcon from "./assets/bulb-circle.svg"
+import xIcon from "./assets/x-icon.svg"
+import facebookIcon from "./assets/facebook-icon.svg"
+import redditIcon from "./assets/reddit-icon.svg" 
+import copyIcon from "./assets/copy-icon.svg"
 
 function Sidebar() {
   return (
-    <aside>
-        <button className='close'>close button</button>
+    <div className="sidebar-overlay overlay-bg">
+       <aside>
+        <button className='close clickable' >
+            <Image src={closeIcon} alt='close' />
+        </button>
 
         <div className="sidebar-contents">
             <div className="sidebar-image-and-heading">
-                <Image alt='image of' src={""}/>
+                <div className="sidebar-image-container">
+                <Image className='main-sidebar-image' alt='image of' src={placeholderImage}/>
+                </div>
 
                 <div className='sidebar-heading'>
                     <div className="sidebar-heading-top">
                         <div className="left">
                             <h1>Slave</h1>
                             <small>/slav/</small>
-                            <Image src={""} alt='speaker icon'/>
+                            <Image src={speakerIcon} alt='speaker icon'/>
                         </div>
 
                         <div className="right">
-                            <button>Bookmark Icon</button>
+                            <button className='clickable'>
+                                <Image
+                                src={bookmarkIcon}
+                                alt="bookmark icon"
+                                />
+                            </button>
                         </div>
                     </div>
 
                     <div className="h-divider"></div>
 
                     <div className="sidebar-heading-bottom">
-                        <Image src={""} alt='flag icon'/>
+                        <Image 
+                        className='flag-icon'
+                        src={flagIcon} alt='flag icon'/>
 
                         <span className="v-divider"></span>
 
@@ -41,18 +64,25 @@ function Sidebar() {
             <div className="other-sidebar-content">
                 <div className="meaning">
                     <h4>ORIGINAL MEANING</h4>
+
                     <p>A person that comes from the Slavic group</p>
                 </div>
 
                 <div className="meaning">
                     <h4>MODERN MEANING</h4>
+
                     <p>A person who is without freedom of any sort i.e movement, expression</p>
                 </div>
 
                 <div className="sidebar-insights">
-                    <Image alt='bulb icon' src={""}/>
+                    <div className="insight-long"></div>
+
+                    <div className="insight-text">
+                         <Image alt='bulb icon' src={bulbIcon}/>
 
                     <p>From Medieval Latin sclavus, referred to Slavic people who during early medieval conflicts, were frequently captured and traded across Europe, eventually giving rise to the generalized term for an unfree person held in servitude </p>
+                    </div>
+                   
                 </div>
 
                 <div className="sidebar-language-history">
@@ -65,11 +95,9 @@ function Sidebar() {
                             <p>11AD</p>
                             <p>11AD</p>
                             <p>11AD</p>
-                            <p>11AD</p>
                         </div>
 
                         <div className="line-with-dots">
-                            <div className="dot"></div>
                             <div className="dot"></div>
                             <div className="dot"></div>
                             <div className="dot"></div>
@@ -131,18 +159,29 @@ function Sidebar() {
                     <h2>SHARE</h2>
 
                     <div className="share-links">
-                        <button>X</button>
-                        <button>Facebook</button>
-                        <button>Reddit</button>
                         <button>
-                            <Image src={""} alt='copy icon'/>
+                            <Image alt='x icon' src={xIcon} />
+                        </button>
+                        
+                        <button>
+                            <Image alt='facebook icon' src={facebookIcon} />
+                        </button>
+
+                        <button>
+                            <Image alt='reddit icon' src={redditIcon} />
+                        </button>
+
+                        <button className='copy'>
+                            <Image src={copyIcon} alt='copy icon'/>
                             Copy Link
                         </button>
                     </div>
                 </div>
             </div>
         </div>
-    </aside>
+    </aside> 
+    </div>
+    
   )
 }
 
